08d972b1e6c60b519cab4c3f4cb5b90aeb55d22d
WGB5445 <919603023@qq.com>
2023-12-14 04:13:57 +0800

    Donate3SBT

--- test/Donate3SBT.t.sol
+++ test/Donate3SBT.t.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {Donate3SBT} from "../src/Donate3SBT.sol";
 
 contract CounterTest is Test {
@@ -9,7 +10,12 @@ contract CounterTest is Test {
     address public owner;
 
     function setUp() public {
-        donate3SBT = new Donate3SBT(address(this), "Donate3SBT", "DBT");
+        donate3SBT = new Donate3SBT(
+            address(this),
+            "Donate3SBT",
+            "DBT",
+            "12345/"
+        );
         owner = address(0x1234);
     }
 
@@ -28,5 +34,10 @@ contract CounterTest is Test {
         donate3SBT.mintTo(owner);
         vm.expectRevert();
         donate3SBT.safeTransferFrom(owner, address(0x4321), 0);
+    }
+
+    function test_uri() public {
+        donate3SBT.mintTo(owner);
+        console.log(donate3SBT.tokenURI(0));
     }
 }
